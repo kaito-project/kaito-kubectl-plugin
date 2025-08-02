6460eb4c3ea09aa9b464703628b7625150bcc969
Heba Elayoty <heelayot@microsoft.com>
2025-08-01 19:01:42 -0700

    fix pr-title pipeline
    Signed-off-by: Heba Elayoty <heelayot@microsoft.com>

--- pkg/status_test.go
+++ pkg/status_test.go
@@ -18,10 +18,8 @@ package cmd
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
@@ -48,104 +46,3 @@ func TestStatusCmd(t *testing.T) {
 	})
 }
 
-func TestGetInstanceType(t *testing.T) {
-	tests := []struct {
-		name      string
-		workspace *unstructured.Unstructured
-		expected  string
-	}{
-		{
-			name: "With instance type",
-			workspace: &unstructured.Unstructured{
-				Object: map[string]interface{}{
-					"spec": map[string]interface{}{
-						"resource": map[string]interface{}{
-							"instanceType": "Standard_NC6s_v3",
-						},
-					},
-				},
-			},
-			expected: "Standard_NC6s_v3",
-		},
-		{
-			name: "Missing instance type",
-			workspace: &unstructured.Unstructured{
-				Object: map[string]interface{}{},
-			},
-			expected: "Unknown",
-		},
-	}
-
-	options := &StatusOptions{}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			result := options.getInstanceType(tt.workspace)
-			assert.Equal(t, tt.expected, result)
-		})
-	}
-}
-
-func TestGetConditionStatus(t *testing.T) {
-	tests := []struct {
-		name          string
-		workspace     *unstructured.Unstructured
-		conditionType string
-		expected      string
-	}{
-		{
-			name: "Condition found",
-			workspace: &unstructured.Unstructured{
-				Object: map[string]interface{}{
-					"status": map[string]interface{}{
-						"conditions": []interface{}{
-							map[string]interface{}{
-								"type":   "ResourceReady",
-								"status": "True",
-							},
-						},
-					},
-				},
-			},
-			conditionType: "ResourceReady",
-			expected:      "True",
-		},
-		{
-			name: "Condition not found",
-			workspace: &unstructured.Unstructured{
-				Object: map[string]interface{}{},
-			},
-			conditionType: "ResourceReady",
-			expected:      "Unknown",
-		},
-	}
-
-	options := &StatusOptions{}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			result := options.getConditionStatus(tt.workspace, tt.conditionType)
-			assert.Equal(t, tt.expected, result)
-		})
-	}
-}
-
-func TestGetAge(t *testing.T) {
-	now := time.Now()
-	oneHourAgo := now.Add(-time.Hour)
-
-	workspace := &unstructured.Unstructured{
-		Object: map[string]interface{}{
-			"metadata": map[string]interface{}{
-				"creationTimestamp": oneHourAgo.Format(time.RFC3339),
-			},
-		},
-	}
-
-	options := &StatusOptions{}
-	result := options.getAge(workspace)
-
-	// Should contain some time duration
-	assert.NotEqual(t, "<unknown>", result)
-	assert.NotEmpty(t, result)
-}
